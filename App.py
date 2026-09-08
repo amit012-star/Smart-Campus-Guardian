1d48ecd73b6c435adc8fab22262d1b271c8258d2
amit012-star <amitbhai6230853958@gmail.com>
2026-09-09 01:17:57 +0530

    Fix formatting of location details input placeholder

--- App.py
+++ App.py
@@ -147,10 +147,10 @@ def student_dashboard():
         ]
     )
      location_details = st.text_input(
-     "📍 Location Details",
-     placeholder="Example: 2nd floor, near Room 204"
+        "📍 Location Details",
+        placeholder="Example: 2nd floor,
+     near Room 204"
     )
-
     description = st.text_area(
         "Describe the Emergency"
     )
