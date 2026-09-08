cce313b346eb226f5ad4796a34f5eee03930269e
amit012-star <amitbhai6230853958@gmail.com>
2026-09-09 00:12:04 +0530

    Add SQLite database system
    Implemented functions to create a database, add emergencies, and retrieve emergency records.

---
+++ database.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+
+def create_database():
+
+    connection = sqlite3.connect("emergency.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS emergencies (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_name TEXT NOT NULL,
+            emergency_type TEXT NOT NULL,
+            location TEXT NOT NULL,
+            description TEXT NOT NULL,
+            priority TEXT NOT NULL,
+            status TEXT NOT NULL,
+            created_at TEXT NOT NULL
+        )
+    """)
+
+    connection.commit()
+
+    connection.close()
+
+
+def add_emergency(
+    student_name,
+    emergency_type,
+    location,
+    description,
+    priority,
+    status,
+    created_at
+):
+
+    connection = sqlite3.connect("emergency.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO emergencies
+        (
+            student_name,
+            emergency_type,
+            location,
+            description,
+            priority,
+            status,
+            created_at
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (
+        student_name,
+        emergency_type,
+        location,
+        description,
+        priority,
+        status,
+        created_at
+    ))
+
+    connection.commit()
+
+    connection.close()
+
+
+def get_emergencies():
+
+    connection = sqlite3.connect("emergency.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT *
+        FROM emergencies
+        ORDER BY id DESC
+    """)
+
+    data = cursor.fetchall()
+
+    connection.close()
+
+    return data
